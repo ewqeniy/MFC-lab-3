f198b18304befd39f45690744be0a342a22226f3
ewqeniy <efnnov@yandex.com>
2018-12-08 03:09:40 +0300

    CDC + changeCursor lab

--- MFC lab 3/MFC lab 3Dlg.cpp
+++ MFC lab 3/MFC lab 3Dlg.cpp
@@ -30,6 +30,12 @@ void CMFClab3Dlg::DoDataExchange(CDataExchange* pDX)
 BEGIN_MESSAGE_MAP(CMFClab3Dlg, CDialogEx)
 	ON_WM_PAINT()
 	ON_WM_QUERYDRAGICON()
+	ON_WM_RBUTTONDOWN()
+	ON_WM_RBUTTONUP()
+	ON_WM_MOUSEMOVE()
+	ON_WM_LBUTTONDOWN()
+	ON_WM_LBUTTONUP()
+	ON_WM_KEYDOWN()
 END_MESSAGE_MAP()
 
 
@@ -45,6 +51,7 @@ BOOL CMFClab3Dlg::OnInitDialog()
 	SetIcon(m_hIcon, FALSE);		// Мелкий значок
 
 	// TODO: добавьте дополнительную инициализацию
+	
 
 	return TRUE;  // возврат значения TRUE, если фокус не передан элементу управления
 }
@@ -85,3 +92,76 @@ HCURSOR CMFClab3Dlg::OnQueryDragIcon()
 	return static_cast<HCURSOR>(m_hIcon);
 }
 
+
+
+void CMFClab3Dlg::OnRButtonDown(UINT nFlags, CPoint point)
+{
+	// TODO: Add your message handler code here and/or call default
+	flagR = true;
+	m_StartPoint = point;
+	CDialogEx::OnRButtonDown(nFlags, point);
+}
+
+
+void CMFClab3Dlg::OnRButtonUp(UINT nFlags, CPoint point)
+{
+	// TODO: Add your message handler code here and/or call default
+	flagR = false;
+	m_EndPoint = point;
+	CDialogEx::OnRButtonUp(nFlags, point);
+}
+
+
+void CMFClab3Dlg::OnMouseMove(UINT nFlags, CPoint point)
+{
+	// TODO: Add your message handler code here and/or call default
+	CClientDC dc(this);
+	if (flagR) {
+		CPen pen(PS_SOLID,30,RGB(0,0,255));
+		CBrush brush(RGB(0, 0, 255));
+		dc.SelectObject(pen);
+		dc.SelectObject(brush);
+		dc.Ellipse(point.x + 10, point.y + 10, point.x-10, point.y-10);
+	}
+	if (flagL) {
+		CPen pen(PS_SOLID, 1, RGB(0, 0, 0));
+		CBrush brush(RGB(0, 0, 0));
+		dc.SelectObject(pen);
+		dc.SelectObject(brush);
+		dc.MoveTo(m_StartPoint);
+		m_StartPoint = point;
+		dc.LineTo(point);
+	}
+	CDialogEx::OnMouseMove(nFlags, point);
+}
+
+
+void CMFClab3Dlg::OnLButtonDown(UINT nFlags, CPoint point)
+{
+	// TODO: Add your message handler code here and/or call default
+	flagL = true;
+	m_StartPoint = point;
+	CDialogEx::OnLButtonDown(nFlags, point);
+}
+
+
+void CMFClab3Dlg::OnLButtonUp(UINT nFlags, CPoint point)
+{
+	// TODO: Add your message handler code here and/or call default
+	flagL = false;
+	m_EndPoint = point;
+	CDialogEx::OnLButtonUp(nFlags, point);
+}
+
+
+void CMFClab3Dlg::OnKeyDown(UINT nChar, UINT nRepCnt, UINT nFlags)
+{
+	// TODO: Add your message handler code here and/or call default
+	switch (nChar)
+	{
+		case(0x51): SetCursor(::LoadCursor(NULL, IDC_ARROW));
+		case(0x57): SetCursor(::LoadCursor(NULL, IDC_SIZEALL));
+		case(0x45): SetCursor(::LoadCursor(NULL, IDC_CROSS));
+	}
+	CDialogEx::OnKeyDown(nChar, nRepCnt, nFlags);
+}
--- MFC lab 3/MFC lab 3Dlg.h
+++ MFC lab 3/MFC lab 3Dlg.h
@@ -30,4 +30,14 @@ protected:
 	afx_msg void OnPaint();
 	afx_msg HCURSOR OnQueryDragIcon();
 	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnRButtonDown(UINT nFlags, CPoint point);
+	afx_msg void OnRButtonUp(UINT nFlags, CPoint point);
+	CPoint m_StartPoint, m_EndPoint;
+	bool flagR = false;
+	bool flagL = false;
+	afx_msg void OnMouseMove(UINT nFlags, CPoint point);
+	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
+	afx_msg void OnLButtonUp(UINT nFlags, CPoint point);
+	afx_msg void OnKeyDown(UINT nChar, UINT nRepCnt, UINT nFlags);
 };
